54dc0891d4ec92c9dc6d235527795fda2f817367
Hugo Duncan <hugo@hugoduncan.org>
2020-09-18 20:47:57 -0400

    Add makejack config

--- doc-src/publish.el
+++ doc-src/publish.el
@@ -1,14 +1,15 @@
 (require 'ox-publish)
 
-(message "default-directory %s" default-directory)
-
 (setq org-export-html-postamble nil)
+
+(setq self-file-name (or buffer-file-name
+                         load-file-name)
+      self-parent-name (or (file-name-directory self-file-name) "./"))
+
 (setq org-publish-project-alist
       `(("docs"
-         :base-directory ,(or (file-name-directory buffer-file-name) "doc-src")
-         :publishing-directory ,(concat
-                                 (or (file-name-directory buffer-file-name) "./")
-                                 "../doc/")
+         :base-directory ,(or (file-name-directory self-file-name) "doc-src")
+         :publishing-directory ,(concat self-parent-name "../doc/")
          :recursive t
          :publishing-function org-html-publish-to-html
          :auto-sitemap t
@@ -16,15 +17,13 @@
          :org-export-html-postamble nil
          )
         ("css"
-         :base-directory ,(concat
-                           (or (file-name-directory buffer-file-name) "./")
-                           "../site-resources/css/")
+         :base-directory ,(concat self-parent-name "../site-resources/css/")
 
          :base-extension "css"
-         :publishing-directory ,(concat
-                                 (or (file-name-directory buffer-file-name) "./")
-                                 "../doc/css/")
+         :publishing-directory ,(concat self-parent-name "../doc/css/")
 
          :publishing-function org-publish-attachment
          :recursive t)
         ("all" :components ("docs" "css"))))
+
+;; (org-publish-all)
--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject criterium "0.4.6-SNAPSHOT"
-  :description "Benchmarking library"
-  :url "https://github.com/hugoduncan/criterium"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :scm {:url "git@github.com:hugoduncan/criterium.git"}
-  :dependencies [[org.openjdk.jmh/jmh-core "1.23"]]
-  :local-repo-classpath true
-  :java-source-paths ["src"])
